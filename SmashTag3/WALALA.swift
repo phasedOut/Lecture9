eedfe6b15600de6de013e05b41f92c5532e7d22a
ysakai <yujisakai10@gmail.com>
2017-01-14 18:31:16 +0900

    file aded

---
+++ SmashTag3/WALALA.swift
@@ -0,0 +1,24 @@
+//
+//  WALALA.swift
+//  SmashTag3
+//
+//  Created by Yuji Sakai on 1/14/17.
+//  Copyright © 2017 Yuji Sakai. All rights reserved.
+//
+
+import UIKit
+
+class WALALA: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
