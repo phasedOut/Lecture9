2ca7aea38f9338335df3b83523c1a12f2c1ffbbc
ysakai <yujisakai10@gmail.com>
2017-01-25 19:09:15 +0900

    coredatastack added

--- SmashTag3/AppDelegate.swift
+++ SmashTag3/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    lazy var coreDataStack = CoreStackData()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -43,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         SearchHistory.History.save()
+        coreDataStack.saveContext()
     }
 
 
