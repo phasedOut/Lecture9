40e9d7f66ebafee5635dcad602930e3ac89d42a1
ysakai <yujisakai10@gmail.com>
2017-01-28 07:27:13 +0900

    update

--- SmashTag3/CoreStackData.swift
+++ SmashTag3/CoreStackData.swift
@@ -18,7 +18,7 @@ class CoreStackData {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "Foo1")
+        let container = NSPersistentContainer(name: "Model")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
